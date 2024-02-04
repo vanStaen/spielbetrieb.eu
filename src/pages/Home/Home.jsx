2a178f1ff715841c3550199aa329208a3cc9e1a3
vanStaen <clement.vanstaen@gmail.com>
2024-02-04 12:52:40 +0100

    skeleton WIP

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import { Tooltip } from 'antd';
 import { useTranslation } from 'react-i18next';
-import {
-  MailOutlined,
-  LinkOutlined
-} from '@ant-design/icons';
+import { MailOutlined } from '@ant-design/icons';
+import { observer } from 'mobx-react';
 
 import { NewsletterForm } from '../../components/NewsletterForm/NewsletterForm';
 import { DarkModeDropDown } from '../../components/DarkModeDropDown/DarkModeDropDown';
 import { LanguageDropDown } from '../../components/LanguageDropDown/LanguageDropDown';
 import { Header } from './Header/Header';
+import { pageStore } from '../../store/pageStore/pageStore';
 
 import './Home.less';
 
-export const Home = () => {
+export const Home = observer(() => {
   const { t } = useTranslation();
 
   return (
@@ -23,7 +22,11 @@ export const Home = () => {
       <Header />
       <div className="background invertColorTheme" id="background"></div>
       <div className="home__container">
-        <div className="home__subContainer">
+        <div className="home__subContainer" style={{
+          backgroundColor: pageStore.selectedTheme === 'dark' 
+          ? 'rgba(20,36,54,.85)' 
+          : 'rgba(225,195,180,.8)'
+        }}>
           <div
             className="home__logoAndText invertColorTheme"
             id="spielbetrieb"
@@ -53,4 +56,4 @@ export const Home = () => {
       </div>
     </>
   );
-};
+});
