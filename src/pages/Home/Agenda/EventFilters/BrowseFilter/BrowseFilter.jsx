dd7562416e988d2b1741c5fe8505e63ade2aa69f
vanStaen <clement.vanstaen@gmail.com>
2024-02-13 12:22:11 +0100

    browseFilter

---
+++ src/pages/Home/Agenda/EventFilters/BrowseFilter/BrowseFilter.jsx
@@ -0,0 +1,112 @@
+import React, { useEffect, useState } from "react";
+import { observer } from 'mobx-react';
+import { 
+    CaretLeftOutlined, 
+    CaretRightOutlined,
+} from '@ant-design/icons';
+import * as dayjs from 'dayjs';
+import * as advancedFormat from 'dayjs/plugin/advancedFormat';
+import * as isoWeek from 'dayjs/plugin/isoWeek';
+
+import { pageStore } from "../../../../../store/pageStore/pageStore";
+
+import './BrowseFilter.less';
+
+const DATE_FORMAT_MONTH = 'MMM, YYYY';
+const DATE_FORMAT_CW = 'ww, YYYY';
+const DATE_FORMAT_DAY = 'DD MMM, YYYY';
+
+export const BrowseFilter = observer(() => {
+    const [showFormatMenu, setShowFormatMenu] = useState(false);
+    const [filterFormat, setFilterFormat] = useState('month'); // Day, Week, Month
+    const [filterDateFrom, setFilterDateFrom] = useState(dayjs())
+    const [filterText, setFilterText] = useState(dayjs().format(DATE_FORMAT_MONTH))
+
+    const handleChangeZeitRaumClick = (add) => {
+        let newFilterDateFrom;
+        if (add) {
+            newFilterDateFrom = dayjs(filterDateFrom).add(1, filterFormat);
+        } else {
+            newFilterDateFrom = dayjs(filterDateFrom).subtract(1, filterFormat);
+        }            
+        setFilterDateFrom(newFilterDateFrom);
+        updateFilterFormatDisplay(filterFormat, newFilterDateFrom);
+    }
+
+    const filterFormatChange = (newFormat) => {
+        console.log('newFormat', newFormat);
+        setFilterFormat(newFormat);
+        updateFilterFormatDisplay(newFormat, filterDateFrom);
+        handleHideMenu();
+    }
+
+    const updateFilterFormatDisplay = (newfilterFormat, newFilterDateFrom) => {
+        let browseFilterText;
+        if (newfilterFormat === 'month') { 
+            browseFilterText = newFilterDateFrom.format(DATE_FORMAT_MONTH)
+        }
+        else if (newfilterFormat === 'week') { 
+            browseFilterText = <><span style={{opacity: .5}}>Week </span>{newFilterDateFrom.format(DATE_FORMAT_CW)}</>
+        }
+        else if (newfilterFormat === 'day') {
+            browseFilterText = newFilterDateFrom.format(DATE_FORMAT_DAY)
+        }
+        setFilterText(browseFilterText);
+    }
+
+    const handleHideMenu = () => {
+        const elementContainer = document.getElementById('browseFilter__menuContainer');
+        elementContainer.style.opacity = 0;
+        setTimeout(function () {
+            setShowFormatMenu(false);
+        }, 300);
+    };
+
+    return (
+            <div className='browseFilter__container'>
+                <div className="browseFilter__text">
+                    <CaretLeftOutlined 
+                        className='browseFilter__logo'
+                        onClick={() => handleChangeZeitRaumClick(false)}
+                    />{" "}
+                    <span onClick={() => setShowFormatMenu(!showFormatMenu)}>{ filterText }</span>{" "}
+                    <CaretRightOutlined 
+                        className='browseFilter__logo'
+                        onClick={() => handleChangeZeitRaumClick(true)}
+                    />
+                </div>
+                {showFormatMenu && 
+                    <>
+                        <div
+                            className="browseFilter__silentBackground"
+                            onClick={handleHideMenu}
+                        >
+                        </div>
+                        <div
+                            className={`browseFilter__menuContainer ${pageStore.selectedTheme === 'light' ? 'lightColorTheme__Menu' : 'darkColorTheme__Menu'}`}
+                            id="browseFilter__menuContainer"
+                        >
+                            <div 
+                                className={`browseFilter__menuElement menu__element ${filterFormat === 'month' && 'menu__elementSelected'}`}
+                                onClick={() => filterFormatChange("month")}
+                            >
+                                {dayjs(filterDateFrom).format(DATE_FORMAT_MONTH)}
+                            </div>
+                            <div 
+                                className={`browseFilter__menuElement menu__element ${filterFormat === 'week' && 'menu__elementSelected'}`}
+                                onClick={() => filterFormatChange("week")}
+                            >
+                                Week {dayjs(filterDateFrom).format(DATE_FORMAT_CW)}
+                            </div>
+                            <div 
+                                className={`browseFilter__menuElement menu__element ${filterFormat === 'day' && 'menu__elementSelected'}`}
+                                onClick={() => filterFormatChange("day")}
+                            >
+                                {dayjs(filterDateFrom).format(DATE_FORMAT_DAY)}
+                            </div>
+                        </div>
+                    </>
+                }
+            </div>
+        )
+})
--- src/pages/Home/Agenda/EventFilters/EventFilters.jsx
+++ src/pages/Home/Agenda/EventFilters/EventFilters.jsx
@@ -9,33 +9,11 @@ import * as advancedFormat from 'dayjs/plugin/advancedFormat';
 import * as isoWeek from 'dayjs/plugin/isoWeek';
 
 import { pageStore } from "../../../../store/pageStore/pageStore";
+import { BrowseFilter } from "./BrowseFilter/BrowseFilter";
 
 import "./EventFilters.less";
 
 export const EventFilters = observer(() => {
-    const [filterFormat, setFilterFormat] = useState('month'); // Day, Week, Month
-    const [filterZeitRaumDate, setFilterZeitRaumDate] = useState(dayjs())
-    const [filterZeitRaumText, setFilterZeitRaumText] = useState(dayjs().format('MMM, YYYY'))
-
-    const filterFormatChange = () => {
-
-    }
-
-    const handleChangeZeitRaumClick = (add) => {
-        let newFilterZeitRaumDate;
-        let dateFormat;
-        if (add) {
-            newFilterZeitRaumDate = dayjs(filterZeitRaumDate).add(1, filterFormat);
-        } else {
-            newFilterZeitRaumDate = dayjs(filterZeitRaumDate).subtract(1, filterFormat);
-        }            
-        setFilterZeitRaumDate(newFilterZeitRaumDate);
-        if (filterFormat === 'month') { dateFormat = 'MMM, YYYY' }
-        else if (filterFormat === 'week') { dateFormat = 'ww, YYYY' }
-        else if (filterFormat === 'day') { dateFormat = 'DD MMM, YYYY' }
-        setFilterZeitRaumText(newFilterZeitRaumDate.format(dateFormat));
-
-    }
 
     return (
         <div className={`agenda__filterContainer ${pageStore.selectedTheme === "light" ? 'lightColorTheme__Text' : 'darkColorTheme__Text'}`}>
@@ -43,15 +21,7 @@ export const EventFilters = observer(() => {
                 here come filters & search
             </div>
             <div className='agenda__browseZeitRaum'>
-                <CaretLeftOutlined 
-                    className='agenda__browseZeitRaumLogo'
-                    onClick={() => handleChangeZeitRaumClick(false)}
-                />{" "}
-                { filterZeitRaumText }{" "}
-                <CaretRightOutlined 
-                    className='agenda__browseZeitRaumLogo'
-                    onClick={() => handleChangeZeitRaumClick(true)}
-                />
+                <BrowseFilter />
             </div>
         </div>
     )
