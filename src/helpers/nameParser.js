97d9fc6aa72a5c92b39c9ae5c35cd03336d487c0
vanStaen <clement.vanstaen@gmail.com>
2024-02-16 19:49:41 +0100

    linter ran

--- src/helpers/nameParser.js
+++ src/helpers/nameParser.js
@@ -1,7 +1,7 @@
 export const nameParser = (json, language) => {
-  if (typeof(json) === "undefined") {
-    return
+  if (typeof json === "undefined") {
+    return;
   }
   const obj = JSON.parse(json);
   return obj[language];
-}
+};
