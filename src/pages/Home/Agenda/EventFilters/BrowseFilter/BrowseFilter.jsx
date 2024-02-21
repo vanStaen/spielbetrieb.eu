9a583eb25208361776c76cc724c59749cfe250be
vanStaen <clement.vanstaen@gmail.com>
2024-02-21 17:56:44 +0100

    get rid of useNavigate

--- src/pages/Home/Agenda/EventFilters/BrowseFilter/BrowseFilter.jsx
+++ src/pages/Home/Agenda/EventFilters/BrowseFilter/BrowseFilter.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams, } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import { observer } from "mobx-react";
 import {
@@ -19,7 +19,6 @@ import "./BrowseFilter.less";
 
 export const BrowseFilter = observer(() => {
   const { t } = useTranslation();
-  const navigate = useNavigate();
   const params = useParams();
 
   const [filterText, setFilterText] = useState(dayjs().format(DATE_FORMAT_MONTH));
