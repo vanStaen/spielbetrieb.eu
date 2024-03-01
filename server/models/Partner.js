d49524fecc2a897fbd1f8388e24098b23bcd2995
vanStaen <clement.vanstaen@gmail.com>
2024-03-01 13:48:17 +0100

    partner model WIP

---
+++ server/models/Partner.js
@@ -0,0 +1,67 @@
+const { sequelize, DataTypes } = require("../lib/sequelizedb");
+const { User } = require("./User");
+
+const Partner = sequelize.define("partner", {
+  _id: {
+    type: DataTypes.INTEGER,
+    field: "_id",
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  avatar: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  pictures: {
+    type: DataTypes.ARRAY(DataTypes.STRING),
+    defaultValue: [],
+  },
+  profilSettings: {
+    type: DataTypes.STRING,
+    allowNull: true,
+    defaultValue: null,
+  },
+  reviews: {
+    type: DataTypes.ARRAY(DataTypes.STRING),
+    allowNull: true,
+  },
+  lastActive: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+  partnerRoles: {
+    type: DataTypes.ARRAY(DataTypes.STRING),
+    defaultValue: [],
+  },
+  partnertype: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  archived: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+  deleted: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+  suspended: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+});
+
+User.hasMany(Partner);
+Partner.belongsTo(User);
+
+module.exports = {
+  Partner,
+};
