d6f24f754e6600fb2f042758e04df9534610f0ed
vanStaen <clement.vanstaen@gmail.com>
2024-04-18 17:08:03 +0200

    linter

--- src/pages/Home/Event/EventPage/EventPageInValidation/EventPageInValidation.jsx
+++ src/pages/Home/Event/EventPage/EventPageInValidation/EventPageInValidation.jsx
@@ -1,30 +1,23 @@
 import React from "react";
 import { observer } from "mobx-react";
 
-import waitLogo from '../../../../../img/logos/waitLogo.png'
+import waitLogo from "../../../../../img/logos/waitLogo.png";
 import { pageStore } from "../../../../../store/pageStore/pageStore";
 
 import "./EventPageInValidation.less";
 
 export const EventPageInValidation = observer(() => {
-    return (
-        <div
-            className={`eventpagevalidated__container ${pageStore.selectedTheme === "light" ? "lightColorTheme__Text" : "darkColorTheme__Text"}`}
-        >
-            <div className="eventpagevalidated__text">
-                <img
-                    src={waitLogo}
-                    width="50px"
-                    className="logo invertColorTheme"
-                />
-                <div className="text">
-                    Event pending review
-                </div>
-                <div className="subText">
-                    This will be available after it has been reviewed by
-                    our team.
-                </div>
-            </div>
+  return (
+    <div
+      className={`eventpagevalidated__container ${pageStore.selectedTheme === "light" ? "lightColorTheme__Text" : "darkColorTheme__Text"}`}
+    >
+      <div className="eventpagevalidated__text">
+        <img src={waitLogo} width="50px" className="logo invertColorTheme" />
+        <div className="text">Event pending review</div>
+        <div className="subText">
+          This will be available after it has been reviewed by our team.
         </div>
-    );
+      </div>
+    </div>
+  );
 });
