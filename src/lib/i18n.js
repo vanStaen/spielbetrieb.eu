d927636677bf30eb1a0455fc4276262b58c163d1
vanStaen <clement.vanstaen@gmail.com>
2024-04-21 23:44:14 +0200

    fix i18n on prod

--- src/lib/i18n.js
+++ src/lib/i18n.js
@@ -8,7 +8,7 @@ i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    debug: false,
+    debug: true,
     fallbackLng: "EN",
     partialBundledLanguages: true,
     interpolation: {
