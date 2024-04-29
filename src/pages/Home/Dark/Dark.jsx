f9786321f68c7e9dcd37d651f32fcad18fdc6715
vanStaen <clement.vanstaen@gmail.com>
2024-04-29 21:53:54 +0200

    fix UX bug eventpage

--- src/pages/Home/Dark/Dark.jsx
+++ src/pages/Home/Dark/Dark.jsx
@@ -24,7 +24,8 @@ export const Dark = observer(() => {
         className={`dark__intro ${pageStore.selectedTheme === "light" ? "lightColorTheme__SubText" : "darkColorTheme__SubText"}`}
       >
         <div className="dark__introTitle">DARK is ...</div>
-        ... a lifestyle magazin made in Berlin, themed around BDSM fetish and kink. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        ... a lifestyle magazin made in Berlin, themed around BDSM fetish and
+        kink. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
         eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
         minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
         ex ea commodo consequat.
