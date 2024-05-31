2c301918b7879505b62db1f22817bc80a04d5c50
vanStaen <clement.vanstaen@gmail.com>
2024-05-31 10:57:03 +0200

    placeholder pic

--- src/pages/Home/Spielplan/EventCard/pictureOrPlaceholder.js
+++ src/pages/Home/Spielplan/EventCard/pictureOrPlaceholder.js
@@ -2,12 +2,18 @@ import { getPictureUrl } from "../../../../helpers/picture/getPictureUrl.js";
 import eventPlaceholder from "../../../../img/artworks/eventPlaceholder.jpg";
 import kitkatPlaceholder from "../../../../img/artworks/kitkatclub.png";
 import insomniaPlaceholder from "../../../../img/artworks/insomniaberlin.jpg";
+import symbiotikka from "../../../../img/artworks/symbiotikka.png";
+import electricmonday from "../../../../img/artworks/electricmonday.png";
 
 export function pictureOrPlaceholder(event) {
   if (event.pictures[0]) {
     return getPictureUrl(event.pictures[0], "events");
   } else if (event.externalPicture) {
     return event.externalPicture;
+  } else if (event.location === 1 && event.title.toLowerCase().includes('symbiotikka')) {
+    return symbiotikka;
+  } else if (event.location === 1 && event.title.toLowerCase().includes('electric monday')) {
+    return electricmonday;
   } else if (event.location === 1) {
     return kitkatPlaceholder;
   } else if (event.location === 8) {
