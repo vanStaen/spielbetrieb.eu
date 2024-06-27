1defd8e35f3678cd58d011571e684a8a45c72b6d
vanStaen <clement.vanstaen@gmail.com>
2024-06-28 00:53:56 +0200

    make sure that tags are always populated

--- src/pages/Home/Profile/ProfileMain/ProfileTags/ProfileTags.jsx
+++ src/pages/Home/Profile/ProfileMain/ProfileTags/ProfileTags.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { observer } from "mobx-react";
 import { useTranslation } from "react-i18next";
 import { Tag } from "antd";
@@ -15,7 +15,7 @@ export const ProfileTags = observer(() => {
   const { t } = useTranslation();
   const [showTagsModal, setShowTagsModal] = useState(false);
 
-  const userTags = () => {
+  const createUserTags = () => {
     const userTags = profileStore.tags?.map((tagId) => {
       return {
         name: nameParser(
@@ -35,6 +35,12 @@ export const ProfileTags = observer(() => {
     return tagsFormatted;
   };
 
+  const [userTags, setUserTags] = useState(createUserTags());
+
+  useEffect(() => {
+    setUserTags(createUserTags());
+  }, [profileStore.tags]);
+
   return (
     <>
       <EditTagsModal
@@ -50,7 +56,7 @@ export const ProfileTags = observer(() => {
         />
         <div className="profileTags__main">
           {profileStore.tags.length ? (
-            <div>{userTags()}</div>
+            <div>{userTags}</div>
           ) : (
             <div className="profileTags__empty">{t("profile.nothingYet")}</div>
           )}
