3670ba9ca8c69dc9337e4d9454935b2b3410ce35
vanStaen <clement.vanstaen@gmail.com>
2024-07-10 15:28:43 +0200

    Hide pending Partner to other user

--- src/pages/Home/Profile/ProfileMain/ProfilePartners/ProfilePartners.jsx
+++ src/pages/Home/Profile/ProfileMain/ProfilePartners/ProfilePartners.jsx
@@ -3,6 +3,7 @@ import { observer } from "mobx-react";
 import { useTranslation } from "react-i18next";
 
 import { profileStore } from "../../../../../store/profileStore/profileStore";
+import { userStore } from "../../../../../store/userStore/userStore";
 import { ProfileMainTitle } from "../profileComponents/ProfileMainTitle/ProfileMainTitle";
 import { CreatePartnerForm } from "./CreatePartnerForm/CreatePartnerForm";
 import { PartnerCard } from "./PartnerCard/PartnerCard";
@@ -12,37 +13,45 @@ import "./ProfilePartners.less";
 export const ProfilePartners = observer(() => {
   const [showPartnersModal, setShowPartnersModal] = useState(false);
   const { t } = useTranslation();
+  const thisIsMe = userStore.id === profileStore.id;
 
-  // TODO: add partner form
+  const partnerCards = profileStore.partners.map((partner) => {
+    if (!partner.pending || thisIsMe || userStore.isAdmin) {
+      return <PartnerCard partner={partner} />
+    } else {
+      return null;
+    }
+  })
+  const partnerCardsCleaned = partnerCards?.filter((partner) => partner);
 
   return (
     <>
-      <CreatePartnerForm
-        showModal={showPartnersModal}
-        setShowModal={setShowPartnersModal}
-      />
-      <div className="profilePartners__container">
-        <ProfileMainTitle
-          title={t("profile.partners")}
-          addPartner={true}
-          showEdit={showPartnersModal}
-          setShowEdit={setShowPartnersModal}
-        />
-        <div className="profilePartners__main">
-          {
-            // TODO: make a nice partner card
-            profileStore.partners?.length ? (
-              profileStore.partners.map((partner) => {
-                return <PartnerCard partner={partner} />
-              })
-            ) : (
-              <div className="profilePartners__empty">
-                {t("profile.nothingYet")}
-              </div>
-            )
-          }
+      {thisIsMe &&
+        <CreatePartnerForm
+          showModal={showPartnersModal}
+          setShowModal={setShowPartnersModal}
+        />}
+      {!!(thisIsMe || partnerCardsCleaned?.length) &&
+        <div className="profilePartners__container">
+          <ProfileMainTitle
+            title={t("profile.partners")}
+            addPartner={true}
+            showEdit={showPartnersModal}
+            setShowEdit={setShowPartnersModal}
+          />
+          <div className="profilePartners__main">
+            {
+              profileStore.partners?.length ? (
+                partnerCardsCleaned
+              ) : (
+                <div className="profilePartners__empty">
+                  {t("profile.nothingYet")}
+                </div>
+              )
+            }
+          </div>
         </div>
-      </div>
+      }
     </>
   );
 });
